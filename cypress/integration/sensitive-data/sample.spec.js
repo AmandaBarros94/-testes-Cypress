91c14454bc020fc885ef18742656a2926a73bcc8
amandabarros94 <mandy.bjj1@gmail.com>
2022-09-12 15:30:10 -0300

    protegendo dados sensiveis quando versionado

--- cypress/integration/sensitive-data/sample.spec.js
+++ cypress/integration/sensitive-data/sample.spec.js
@@ -1,10 +1,12 @@
+const cypress = require("cypress")
+
 describe('Sensitive data bad practice', () => {
   beforeEach(() => {
     cy.visit('https://notes-serverless-app.com/login')
   })
 
   it('fills the form with sensitive data', () => {
-    cy.get('#email').type('joe@example.com')
-    cy.get('#password').type('s3Cr€7-p@s5w0rd')
+    cy.get('#email').type(cypress.env('user_email'))
+    cy.get('#password').type(cypress.env('user_password'), { log:false }
   })
 })
