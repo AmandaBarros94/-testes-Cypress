8cff0d30bd5cb3dbd373e92539634c428eb4912f
amandabarros94 <mandy.bjj1@gmail.com>
2022-09-12 11:31:29 -0300

    retirado page objects e adcionando comandos customizados

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+const cypress = require("cypress")
+
 Cypress.Commands.add('search', term => {
   cy.get('input[type="text"]')
     .should('be.visible')
@@ -9,4 +11,15 @@ Cypress.Commands.add('assertResults', () => {
   cy.get('.table-row').then(rows => {
     expect(rows.length).to.be.at.least(1)
   })
+})
+
+cypress.Commands.add('updateDestination', data => {
+  cy.get('#destination_name')
+    .clear()
+    .type(data.name)
+  cy.get('#destination_description')
+    .clear()
+    .type(data.description)
+  cy.get('input[type="submit"]')
+    .click()
 })
